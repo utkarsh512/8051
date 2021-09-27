69aefc4e560002551c01d67e3e91ac89e9998b32
Utkarsh Patel <imutkarshpatel@gmail.com>
2021-09-27 10:58:21 +0530

    Update part_2.asm

--- codes/exp_4/part_2.asm
+++ codes/exp_4/part_2.asm
@@ -1,7 +1,7 @@
 ; Author: Utkarsh Patel (18EC35034)
 ; 
 ; Specification: Ramp Signal Generation
-; Display inverted ramp signal on the DAC Scope. Change the slope of the ramp signal to 30 degrees and 60 degrees
+; Display inverted ramp signal on the DAC Scope. Change the slope of the ramp signal to 30o and 60 o
 ; 
 ; Run this code with update frequency 100
 
@@ -9,7 +9,10 @@ ORG 0000H       ; reset registers
 N EQU 4         ; slope of ramp waveform
 CLR P0.7        ; enable DAC WR line
 
+start:
+  MOV A, #255
+
 loop:          
   MOV P1, A     ; move content of accumulator to P1 for display
-  ADD A, #N     ; increment accumulator by N
+  SUBB A, #N    ; decrement accumulator by N
   JMP loop      ; jump back to loop
